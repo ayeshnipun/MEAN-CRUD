f5cb695d978b5b3508b8e05352cc7b33e2633fa9
ayeshNipun <nipun.yesh@gmail.com>
2019-05-06 19:49:48 +0530

    post request to add data

--- backend/controllers/employeeController.js
+++ backend/controllers/employeeController.js
@@ -13,5 +13,22 @@ router.get('/', (req, res) => {
 	})
 })
 
+router.post('/', (req, res) => {
+	var emp = new Employee({
+		name: req.body.name,
+		position: req.body.position,
+		office: req.body.office,
+		salary: req.body.salary,
+	})
+
+	emp.save((err, doc) => {
+		if (!err) {
+			console.log(doc);
+			res.send(doc);
+		} else {
+			console.log('Error : '+ JSON.stringify(err, undefined, 2));
+		}
+	})
+})
 
 module.exports = router
